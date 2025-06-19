2a00051cc29c8eb21ae996fcfe498fa188666971
officialamar736 <officialamar736@gmail.com>
2025-06-19 11:19:59 +0000

    datatypesDemo.js

---
+++ datatypesdemo.js
@@ -0,0 +1,11 @@
+let productPrice = 150000;
+
+let productdiscount = 15;
+
+let productName = "APPLE I PHONE";
+
+let isAvailable = true;
+console.log(productPrice);
+console.log(productdiscount);
+console.log(productName);
+console.log(isAvailable);
